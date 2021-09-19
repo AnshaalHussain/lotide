676d83027ee95a46e8f9096fe53fecbfc5a36214
Anshaal Hussain <anshaalhussain@gmail.com>
2021-09-18 21:35:37 -0400

    will count the amount of times the letter appears in a sentence

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} !== ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const countLetters = function(sentence) {
+  let count = {}
+  
+  for(letter of sentence ) {
+    if(letter !== " ") {
+      if (count[letter]) {
+        count[letter] += 1;
+      } else {
+        count[letter] = 1
+      }
+    };
+
+  };
+  return count;
+};
+
+
+//console.log(countLetters("lighthouse in the house"));
