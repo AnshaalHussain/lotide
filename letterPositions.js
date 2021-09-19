106357a402437621f6747194fa2b0e7826e5020e
Anshaal Hussain <anshaalhussain@gmail.com>
2021-09-18 23:26:40 -0400

    will find position of letters in a sentnce and return an array of those positions

---
+++ letterPositions.js
@@ -0,0 +1,27 @@
+
+
+const letterPositions = function(string) {
+  let results = {};
+  let count = 0 ;
+
+  for (letter in string) {
+    if(results[string[letter]]) {
+
+      results[string[letter]] + count;
+      results[string[letter]].push(count);
+
+    } else {
+      results[string[letter]] = [];
+      results[string[letter]].push(count);
+    }
+
+    count++;
+    
+  };
+
+  return results;
+};
+
+
+
+console.log(letterPositions("hello"));
