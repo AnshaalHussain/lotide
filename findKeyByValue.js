cbd0178b0f6bce5a63734b4235de4fd3508ff6fa
Anshaal Hussain <anshaalhussain@gmail.com>
2021-09-19 00:26:20 -0400

    finds key based on given value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const findKeyByValue = function (object, val) {
+
+
+  for(key in object) {
+
+    if(object[key] === val) {
+      return [key]
+    }
+
+  }
+  return undefined;
+
+
+};
+
+
+
+
+
+
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
